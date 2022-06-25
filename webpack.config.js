3d50ff30b54aaa57609477c91e92059f9b56851e
n0xx <smc.8645@gmail.com>
2022-06-24 21:37:00 -0500

    Fixed loaders for google-fonts

--- webpack.config.js
+++ webpack.config.js
@@ -6,4 +6,29 @@ module.exports = {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
   },
+  
+  module: {
+    
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'fonts/'
+            }
+          }
+        ]
+      }
+    
+    ],
+
+  },
+
 };
