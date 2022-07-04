237e3ff89dfbc945000e11885711370a67a0c7b5
n0xx <smc.8645@gmail.com>
2022-07-04 16:55:17 -0500

    First commit

---
+++ src/modules/pages/about.js
@@ -0,0 +1,13 @@
+// About page
+
+const about = function() {
+    const container_about = document.createElement('div');
+    container_about.id = 'container-about';
+    //document.getElementById('container') = document.getElementById('container-about');
+    container_about.textContent = 'About section';
+
+    container.appendChild(container_about);
+}
+
+ 
+export default about;
---
+++ src/modules/pages/menu.js
@@ -0,0 +1,14 @@
+// Menu page
+
+const menu = function() {
+    const container_menu = document.createElement('div');
+    
+    container_menu.id = 'container-menu';
+    
+    container_menu.textContent = ' Menu section';
+
+    container.appendChild(container_menu);
+
+}
+
+export default menu;
